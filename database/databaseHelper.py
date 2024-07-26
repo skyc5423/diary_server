1157fd8b706f9bb58322b848978f2e4b399b484c
skyc5423 <skyc5423@kaist.ac.kr>
2024-07-26 09:15:48 +0900

    fix db host

--- database/databaseHelper.py
+++ database/databaseHelper.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database.db_classes import Base
 
-DATABASE_URL = "mysql+mysqlconnector://root:t7835423@localhost/diary"
+DATABASE_URL = "mysql+mysqlconnector://root:t7835423@0.0.0.0/diary"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-TEST_DATABASE_URL = "mysql+mysqlconnector://root:t7835423@localhost/diary_test"
+TEST_DATABASE_URL = "mysql+mysqlconnector://root:t7835423@0.0.0.0/diary_test"
 
 test_engine = create_engine(TEST_DATABASE_URL)
 TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
