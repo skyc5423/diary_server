6dfa81486b274fee4d2fce69911987391c637c13
skyc5423 <skyc5423@kaist.ac.kr>
2024-07-27 13:12:07 +0900

    turn on autoflush and commit

--- database/databaseHelper.py
+++ database/databaseHelper.py
@@ -2,15 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database.db_classes import Base
 
+# TODO: Network error resolving.
 DATABASE_URL = "mysql+mysqlconnector://root:t7835423@0.0.0.0/diary"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=True, autoflush=True, bind=engine)
 
 TEST_DATABASE_URL = "mysql+mysqlconnector://root:t7835423@0.0.0.0/diary_test"
 
 test_engine = create_engine(TEST_DATABASE_URL)
-TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
+TestSessionLocal = sessionmaker(autocommit=True, autoflush=True, bind=test_engine)
 
 Base.metadata.create_all(bind=engine)
 Base.metadata.create_all(bind=test_engine)
