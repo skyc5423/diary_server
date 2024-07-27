5d5298c1f4e859b6d9ebf901467d49336aea3124
skyc5423 <skyc5423@kaist.ac.kr>
2024-07-27 13:31:30 +0900

    fix autocommit to false

--- database/databaseHelper.py
+++ database/databaseHelper.py
@@ -6,12 +6,12 @@ from database.db_classes import Base
 DATABASE_URL = "mysql+mysqlconnector://root:t7835423@0.0.0.0/diary"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=True, autoflush=True, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 TEST_DATABASE_URL = "mysql+mysqlconnector://root:t7835423@0.0.0.0/diary_test"
 
 test_engine = create_engine(TEST_DATABASE_URL)
-TestSessionLocal = sessionmaker(autocommit=True, autoflush=True, bind=test_engine)
+TestSessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=test_engine)
 
 Base.metadata.create_all(bind=engine)
 Base.metadata.create_all(bind=test_engine)
