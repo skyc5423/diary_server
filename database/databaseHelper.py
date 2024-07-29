c989af512be849f5d242f79049aed0f0f2511593
skyc5423 <skyc5423@kaist.ac.kr>
2024-07-29 09:48:49 +0900

    set db engine config to prevent connection error

--- database/databaseHelper.py
+++ database/databaseHelper.py
@@ -5,12 +5,13 @@ from database.db_classes import Base
 # TODO: Network error resolving.
 DATABASE_URL = "mysql+mysqlconnector://root:t7835423@0.0.0.0/diary"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_recycle=14400, pool_size=5, max_overflow=20, echo=False, echo_pool=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 TEST_DATABASE_URL = "mysql+mysqlconnector://root:t7835423@0.0.0.0/diary_test"
 
-test_engine = create_engine(TEST_DATABASE_URL)
+test_engine = create_engine(TEST_DATABASE_URL, pool_recycle=14400, pool_size=1, max_overflow=4, echo=False,
+                            echo_pool=True)
 TestSessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=test_engine)
 
 Base.metadata.create_all(bind=engine)
